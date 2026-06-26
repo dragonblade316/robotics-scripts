7b7f5a9877678b3cc10c63c72deeac4f7493f5a3
Teddy <dragonblade316@gmail.com>
2026-06-25 19:15:50 -0600

    more version stuff

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A set of simple applications to make robot life easier";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: 
